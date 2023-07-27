5bc3e4e55a80e2bad9863f815706bd090f23f79b
Kartik Narang <narangk007@gmail.com>
2023-07-27 10:14:47 +0530

    Bug Solved of Delete Button

--- script.js
+++ script.js
@@ -1,126 +1,108 @@
 var button = document.querySelector('input[type="button"]');
-
 button.addEventListener('click', addExpense);
+
 function addExpense(e) {
     e.preventDefault();
-    //getting the values
+    // Getting the values
     var expenseValue = document.getElementById('amount').value;
     var category = document.getElementById('category').selectedOptions[0].text;
     var description = document.getElementById('description').value;
-    
 
-    //amount-category-description
+    // Creating the display string
     var str = expenseValue + " - " + category + " - " + description + " ";
-    
-    
-    //creating buttons
 
+    // Creating buttons
     var deleteItem = document.createElement('button');
     deleteItem.textContent = 'Delete Item';
 
     var editItem = document.createElement('button');
     editItem.textContent = 'Edit Item';
 
-    //create an ul item
-
+    // Create an ul item
     var ul = document.createElement('ul');
     var li = document.createElement('li');
-    
-    //adding all together
 
+    // Adding all together
     li.textContent = str;
 
-    //adding buttons to li
-
+    // Adding buttons to li
     li.appendChild(deleteItem);
     li.appendChild(editItem);
 
-    //adding li to ul
-
+    // Adding li to ul
     ul.appendChild(li);
 
-
-    //adding ul to html
-
+    // Adding ul to the HTML body
     document.body.appendChild(ul);
 
-    //make the delete button working
+    // Make the delete button working
     deleteItem.addEventListener('click', removeItem);
-    function removeItem(e) {
-        ul.removeChild(li);
-    }
 
-    editItem.addEventListener('click', function() {
+    // Add edit button functionality
+    editItem.addEventListener('click', function () {
         var li = this.parentNode;
         var liData = li.textContent.split(' - ');
-        
-        // create edit form
+
         var form = document.createElement('form');
-        
+
         var amountInput = document.createElement('input');
         amountInput.type = 'text';
         amountInput.name = 'amount';
         amountInput.value = liData[0];
-        
+
         var categoryInput = document.createElement('input');
         categoryInput.type = 'text';
         categoryInput.name = 'category';
         categoryInput.value = liData[1];
-        
+
         var descriptionInput = document.createElement('textarea');
         descriptionInput.name = 'description';
         descriptionInput.textContent = liData[2];
-        
+
         var saveButton = document.createElement('button');
         saveButton.type = 'submit';
         saveButton.textContent = 'Save';
-        
+
         form.appendChild(amountInput);
         form.appendChild(categoryInput);
         form.appendChild(descriptionInput);
         form.appendChild(saveButton);
-        
-        // replace li element with form
+
+        // Replace li element with the form
         li.innerHTML = '';
         li.appendChild(form);
-        
-        // add form submit event listener
-        form.addEventListener('submit', function(e) {
-          e.preventDefault();
-          
-          // get form data
-          var expenseValue = form.elements.amount.value;
-          var category = form.elements.category.value;
-          var description = form.elements.description.value;
-          
-          // update li element with new data
-          var newStr = expenseValue + " - " + category + " - " + description;
-          li.textContent = newStr;
-          
-          // create delete and edit buttons
-          var deleteItem = document.createElement('button');
-          deleteItem.textContent = 'Delete Item';
-      
-          var editItem = document.createElement('button');
-          editItem.textContent = 'Edit Item';
-      
-          li.appendChild(space1);
-          li.appendChild(deleteItem);
-          li.appendChild(space1.cloneNode());
-          li.appendChild(editItem);
-      
-          // make the delete button working
-          deleteItem.addEventListener('click', removeItem);
-          function removeItem(e) {
-              ul.removeChild(li);
-          }
-      
-          // add edit button functionality
-          editItem.addEventListener('click', function() {
-            var li = this.parentNode;
-            var liData = li.textContent.split(' - ');
-            // rest of the code for edit functionality
-          });
+
+        // Add form submit event listener
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            // Get form data
+            var expenseValue = form.elements.amount.value;
+            var category = form.elements.category.value;
+            var description = form.elements.description.value;
+
+            // Update li element with new data
+            var newStr = expenseValue + " - " + category + " - " + description;
+            li.textContent = newStr;
+
+            // Recreate delete and edit buttons
+            li.appendChild(deleteItem);
+            li.appendChild(space1.cloneNode());
+            li.appendChild(editItem);
+
+            // Make the delete button working
+            deleteItem.addEventListener('click', removeItem);
+
+            // Add edit button functionality again
+            editItem.addEventListener('click', function () {
+                // Rest of the edit button functionality here
+            });
         });
-      });
-    }
+    });
+}
+
+function removeItem(e) {
+    var li = e.target.parentNode;
+    var ul = li.parentNode;
+    ul.removeChild(li);
+}
